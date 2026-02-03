d8da07f926773d7f73da2de15f2b380c9fb9ee61
Wesley Murat <wesley.murat@safelabs.com.br>
2026-02-03 17:35:09 -0300

    first commit

---
+++ 05_object_oriented_programming/aula_01_introducao.py
@@ -0,0 +1,6 @@
+"""
+Aula 01 - Introdução
+Playlist: Orientação a Objetos em Python (Remake)
+Autor: Programador Lhama
+"""
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# <h1 align="center">🐍 Python Uuniversity</h1>
+
+---
+
+# <h2 align="center"> 🚧 Em construção 🚧</h2>
